565ad4ecb99d01b423b0da58f73b6740a71fc253
Olga <o.panova27@gmail.com>
2022-07-29 23:22:47 +0300

    Edit function is_bitlink()

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def shorten_link(token, long_url):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    response = requests.post(url_api, json=body, headers=headers)
+    response = requests.post(api_url, json=body, headers=headers)
     response.raise_for_status()
 
-    json_url = json.loads(response.text)
+    json_url = response.json()
     return json_url['link']
 
 def count_clicks(token, bitlink_id):
@@ -23,14 +23,20 @@ def count_clicks(token, bitlink_id):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    response = requests.get(url_api, headers=headers)
+    response = requests.get(api_url, headers=headers)
     response.raise_for_status()
 
-    json_bitlink = json.loads(response.text)
+    json_bitlink = response.json()
     return json_bitlink['total_clicks']
 
-def is_bitlink(url):
-    if "https://bit.ly/" in url:
+def is_bitlink(token, url):
+    bitlink = url.replace("https://","")
+    api_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(api_url, headers=headers)
+    if response.ok:
         return True
 
 if __name__ == "__main__":
@@ -40,7 +46,7 @@ if __name__ == "__main__":
     url = input('Input link: ')
 
     try:
-        if is_bitlink(url):
+        if is_bitlink(token, url):
             bitlink_id = url.replace("https://","")
             count_clicks = count_clicks(token,bitlink_id)
             print(f'Count of clicks: {count_clicks}')
