3cc899eec8d4a36acc6b3eeb91be251a04b8c683
Olga <o.panova27@gmail.com>
2022-07-31 19:49:54 +0300

    Edited main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 import requests
 import os
 
+
 def shorten_link(token, long_url):
     api_url = "https://api-ssl.bitly.com/v4/bitlinks"
 
@@ -15,30 +16,35 @@ def shorten_link(token, long_url):
     response = requests.post(api_url, json=body, headers=headers)
     response.raise_for_status()
 
-    json_url = response.json()
+    url_json = response.json()
     return json_url['link']
 
+
 def count_clicks(token, bitlink_id):
-    api_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink_id}/clicks/summary"
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink_id}/clicks/summary"
 
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    response = requests.get(api_url, headers=headers)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
-    json_bitlink = response.json()
+    bitlink_json = response.json()
+
     return json_bitlink['total_clicks']
 
+
 def is_bitlink(token, url):
-    bitlink = url.replace("https://","")
+    parsed_url = urlparse(url)
+    bitlink = f"{parsed_url.netloc}{parsed_url.path}"
     api_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
     headers = {
         'Authorization': f'Bearer {token}'
     }
     response = requests.get(api_url, headers=headers)
-    if response.ok:
-        return True
+
+    return response.ok
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -49,8 +55,8 @@ if __name__ == "__main__":
     try:
         if is_bitlink(token, url):
             parsed_url = urlparse(url)
-            bitlink_id = parsed_url.netloc+parsed_url.path
-            count_clicks = count_clicks(token,bitlink_id)
+            bitlink_id = f"{parsed_url.netloc}{parsed_url.path}"
+            count_clicks = count_clicks(token, bitlink_id)
             print(f'Count of clicks: {count_clicks}')
         else:
             bitlink_url = shorten_link(token, url)
